9923215ca5d8f8b5c8ccc5cd017f958d58c43d36
Sergio <sramirezpch@gmail.com>
2022-02-21 22:31:13 -0500

    Fetching pickup and drop locations from store

--- src/components/Form.js
+++ src/components/Form.js
@@ -10,6 +10,8 @@ import {
 
 export default function () {
     // const dispatch = useDispatch();
+    const pickupLocation = useSelector(state => state.locations.pickupLocation)
+    const dropoffLocation = useSelector(state => state.locations.dropoffLocation);
 
     // React.useEffect(() => {
     //     async function fetchLocation() {
@@ -20,7 +22,8 @@ export default function () {
 
     function handleSubmit(e) {
         e.preventDefault();
-        console.log('submit');
+        console.log('Pick up location:', pickupLocation);
+        console.log('Drop off location', dropoffLocation);
     }
     return (
         <form onSubmit={handleSubmit} className='w-2/4 px-3 pb-5 flex flex-col gap-y-5'>
